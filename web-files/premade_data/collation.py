886cf53789c5e1eb327e9548b83e99c14f7e3e18
guro199811 <75933749+guro199811@users.noreply.github.com>
2023-07-13 21:43:19 +0400

    deleting nonusable code
    premade data was suppose to fix the collation error, it is not necessery anymore

--- web-files/premade_data/collation.py
+++
@@ -1,6 +0,0 @@
-from ..website import db
-
-db.session.execute('ALTER DATABASE postgres REFRESH COLLATION VERSION;')
-db.session.commit()
-db.session.execute('ALTER DATABASE petsite REFRESH COLLATION VERSION;')
-db.session.commit()
