b3f630a78c36ef11ded63c6dce476e734cd7d6e4
guro199811 <guro.sivs@gmail.com>
2024-11-19 23:34:24 +0400

    feat: optimize bg, add tailwindCSS for home page

--- frontend/reacting-pet/src/app/page.jsx
+++ frontend/reacting-pet/src/app/page.jsx
@@ -1,9 +1,26 @@
 import styles from "./Home.module.css";
+import Image from "next/image";
+// import Image from "../../public/assets/main-bg.jpg"
 
 function Home() {
   return (
-    <div className={styles.homeBg}>
-      <div className={styles.home}>
+    <div
+      className="absolute w-full h-full bg-cover 
+    bg-no-repeat bg-center bg-fixed z-0 items-center justify-center "
+    >
+      <Image
+        src="/assets/main-bg.jpg"
+        alt="Background Image"
+        layout="fill"
+        objectFit="cover"
+        priority
+      />
+      <div
+        className={`${styles.home} relative flex flex-col 
+      items-center justify-center h-auto
+       text-black font-serif border border-black 
+       bg-white/75 rounded-2xl backdrop-blur-sm z-50`}
+      >
         <h1>Welcome to our website!</h1>
         <p>
           Our website is created for your lovely pets and they're veterinary
